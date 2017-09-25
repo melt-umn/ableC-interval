47a952c841c2943051442b81b65d3b6f55a230ce
Patrick Stephen <Sythe2o0@gmail.com>
2017-09-25 10:15:57 -0500

    Compress concrete syntax folder structure (#1)
    Compress concrete syntax folder structure

--- grammars/edu.umn.cs.melt.exts.ableC.interval/concretesyntax/Export.sv
+++
@@ -1,4 +0,0 @@
-grammar edu:umn:cs:melt:exts:ableC:interval:concretesyntax;
-
-exports edu:umn:cs:melt:exts:ableC:interval:concretesyntax:typeExpr;
-exports edu:umn:cs:melt:exts:ableC:interval:concretesyntax:constructor;
